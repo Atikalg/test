94a34287b661468e38af6463d21dd3de6317a1ce
atikalg <who.atik@gmail.com>
2022-04-06 12:15:07 +0530

    Added UC1

---
+++ EmpWageCompUC1.java
@@ -0,0 +1,19 @@
+package com.bridgelab.Assignment4;
+
+/**
+ * @author atik
+ * Checking employee present or absent
+ */
+public class EmpWageCompUC1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage computation program");
+		int isFullTime = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if ( empCheck == isFullTime )
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	}
+
+}
